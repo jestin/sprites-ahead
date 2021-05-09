28b665cd3dfae783854b6cc092d5f9c07ee11846
Jestin Stoffel <jestin.stoffel@gmail.com>
2021-05-09 00:56:58 -0700

    Set up doc manager with template for the palette view

--- app.cpp
+++ app.cpp
@@ -1,11 +1,37 @@
 #include "app.h"
 #include "frame.h"
+#include "SA_document.h"
+#include "SA_palette_view.h"
 
 wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit()
 {
+	m_pDocManager = new wxDocManager();
+
+	// Create a template
+	(void) new wxDocTemplate(
+			m_pDocManager,
+			wxT("Sprites Ahead"),
+			wxT("*.pfh"),
+			wxT(""),
+			wxT("pfh"),
+			wxT("Sprites Ahead Document"),
+			wxT("Palette View"),
+			CLASSINFO(SADocument),
+			CLASSINFO(SAPaletteView));
+
+#ifdef __WXMAC__
+	wxFileName::MacRegisterDefaultTypeAndCreator(wxT("pfh"), 'WXMB', 'WXMA');
+#endif
+
 	MainFrame * frame = new MainFrame();
 	frame->Show(true);
 	return true;
+}
+
+int MyApp::OnExit()
+{
+	delete m_pDocManager;
+	return 0;
 }
--- app.h
+++ app.h
@@ -1,7 +1,12 @@
 #include <wx/wx.h>
+#include <wx/docview.h>
 
 class MyApp : public wxApp
 {
 	public:
 		virtual bool OnInit();
+		virtual int OnExit();
+
+	private:
+		wxDocManager* m_pDocManager;
 };
