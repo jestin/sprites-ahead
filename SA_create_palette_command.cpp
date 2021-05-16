8faa3c5381f14a2edbecaae445b2c191456d3dc8
Jestin Stoffel <jestin.stoffel@gmail.com>
2021-05-15 22:57:59 -0700

    Write code for create palette command

--- SA_create_palette_command.cpp
+++ SA_create_palette_command.cpp
@@ -1,10 +1,13 @@
 #include "SA_create_palette_command.h"
+#include "SA_palette.h"
 
 SACreatePaletteCommand::SACreatePaletteCommand(const wxString& name,
 		CreatePaletteOperation op,
 		SADocument* pDoc)
 	: wxCommand(true, name)
 {
+	m_pDoc = pDoc;
+	m_op = op;
 }
 
 SACreatePaletteCommand::~SACreatePaletteCommand()
@@ -13,10 +16,35 @@ SACreatePaletteCommand::~SACreatePaletteCommand()
 
 bool SACreatePaletteCommand::Do()
 {
-	return true;
+	return DoOrUndo(m_op);
 }
 
 bool SACreatePaletteCommand::Undo()
 {
+	return DoOrUndo(m_op);
+}
+
+bool SACreatePaletteCommand::DoOrUndo(CreatePaletteOperation op)
+{
+	wxASSERT(m_pDoc != NULL);
+
+	switch(op)
+	{
+		case CREATE_PALETTE:
+			// add the new palette
+			m_pDoc->GetPalettes().Append(new SAPalette());
+			break;
+		case UNDO_CREATE_PALETTE:
+			SAPalette* pTemp = (SAPalette *) m_pDoc->GetPalettes().GetLast()->GetData();
+			m_pDoc->GetPalettes().pop_back();
+			delete pTemp;
+			break;
+
+	}
+
+	m_pDoc->Modify(true);
+	m_pDoc->UpdateAllViews();
+
 	return true;
 }
+
